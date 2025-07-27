57f315059efa7a416d9c83947cb94ef0200f22fb
KeerthikaReddy11 <24211a05v4@bvrit.ac.in>
2025-07-27 16:41:18 +0530

    Create characters.c

---
+++ c/characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    char ch,str[100],sen[100];
+    scanf("%c",&ch);
+    scanf("%s",&str);
+    getchar();
+    fgets(sen, sizeof(sen), stdin);
+    printf("%c",ch);
+    printf("\n%s",str);
+    printf("\n%s",sen);
+    return 0;
+}
