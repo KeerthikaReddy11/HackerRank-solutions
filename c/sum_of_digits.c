9d44587d9ba0ff12e479b5e6930a52158b3cb740
KeerthikaReddy11 <24211a05v4@bvrit.ac.in>
2025-07-27 23:01:04 +0530

    Update sum_of_digits.c

--- c/sum_of_digits.c
+++ c/sum_of_digits.c
@@ -2,18 +2,18 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-//Complete the following function.
-
-
-void calculate_the_maximum(int n, int k) {
-  //Write your code here.
-}
 
 int main() {
-    int n, k;
-  
-    scanf("%d %d", &n, &k);
-    calculate_the_maximum(n, k);
- 
+	
+    int n,digit,sum;
+    scanf("%d", &n);
+    sum=0;
+    while(n>0)
+    {
+       digit=n%10;
+       sum=sum+digit;
+       n=n/10;
+    }
+    printf("%d",sum);
     return 0;
 }
